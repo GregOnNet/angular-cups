f82284fe28cd0054b6b72e79d5a32bf3938726e5
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-03 21:10:57 +0100

    Fixing typo

--- src/app/cupper/cupper.js
+++ src/app/cupper/cupper.js
@@ -1,5 +1,5 @@
 (function() {
-  "use strict";
+  'use strict';
 
   angular
     .module('cups.cupper')
@@ -7,7 +7,7 @@
 
   cupper.$inject = [];
 
-  function cupper()){
-    
+  function cupper(){
+
   }
 }());

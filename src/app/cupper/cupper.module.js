0f0ccd427da894abbfe9fe9eb4bf18addd8d9246
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-03 22:22:26 +0100

    Setting cups.core as dependency instead of ngRoute

--- src/app/cupper/cupper.module.js
+++ src/app/cupper/cupper.module.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('cups.cupper', [
-    'ngRoute'
+    'cups.core'
   ]);
 
 }());
--- src/app/cups.module.js
+++ src/app/cups.module.js
@@ -2,6 +2,7 @@
   "use strict";
 
   angular.module('cups', [
+   'cups.dashboard',
    'cups.cupper'
   ]);
 

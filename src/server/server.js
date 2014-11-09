158ef19be192cac80ff64cf976b78550bd802523
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-09 20:30:59 +0100

    Referencing get and post api-action for a cupper

--- src/server/server.js
+++ src/server/server.js
@@ -5,13 +5,22 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var path           = require('path');
 
-var app = express();
-    app.use(express.static(path.join(__dirname , '..' , 'app')));
-    app.use(bodyParser.json());
-    app.use(methodOverride());
-    app.use(bodyParser.urlencoded({'extended':'true'}));
-    app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+var app    = express();
+var router = express.Router();
 
-    app.listen(8080);
+var cupper = require('./cupper/api');
 
+app.use(express.static(path.join(__dirname , '..' , 'app')));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+router.get('/cuppers', cupper.get);
+
+router.post('/cupper', cupper.post);
+
+app.use('/api', router);
+
+app.listen(8080);
 console.log('Listening on port http://localhost:8080...');

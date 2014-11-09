59014603cd6a88a809ccaa21e7b8f9c8be57967c
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-09 22:20:38 +0100

    Adding controller and route for dahsboard

---
+++ src/app/dashboard/dashboard.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('cups.dashboard')
+    .controller('dashboard', dashboard);
+
+  dashboard.$inject = [];
+
+  function dashboard(){
+
+  }
+}());
---
+++ src/app/dashboard/dashboard.routes.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('cups.dashboard')
+    .config(defineRoutes);
+
+  defineRoutes.$inject = ['$routeProvider'];
+
+  function defineRoutes($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'dashboard/dashboard.html',
+        controller: 'dashboard'
+      });
+  }
+}());

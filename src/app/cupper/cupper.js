92eba171cada1ed8d550be8307343be0fab69aa4
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-09 21:15:41 +0100

    Implementing getAll and create for cuppers

--- src/app/cupper/cupper.js
+++ src/app/cupper/cupper.js
@@ -8,6 +8,41 @@
   cupper.$inject = ['$http'];
 
   function cupper($http) {
+    var api = '/api/';
 
+    return {
+      getAll : getAll,
+      create : create
+    };
+
+    function getAll() {
+      $http
+        .get(api + 'cuppers')
+        .then(callSucceeded)
+        .catch(callCrashed);
+
+      function callSucceeded(response){
+        return response.data;
+      }
+
+      function callCrashed(error){
+        console.log(error);
+      }
+    }
+
+    function create(cupper) {
+      $http
+        .post(api + 'cupper', cupper)
+        .then(callSucceeded)
+        .catch(callCrashed);
+
+      function callSucceeded(response){
+        return response.data;
+      }
+
+      function callCrashed(error){
+        console.log(error);
+      }
+    }
   }
 }());

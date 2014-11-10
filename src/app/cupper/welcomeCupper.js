25fcf02dc6757382d782e80f522cd2d6b7e64798
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-10 14:40:28 +0100

    Resetting form after adding a new cupper

--- src/app/cupper/welcomeCupper.js
+++ src/app/cupper/welcomeCupper.js
@@ -5,16 +5,20 @@
     .module('cups.cupper')
     .controller('welcomeCupper', welcomeCupper);
 
-  welcomeCupper.$inject = ['cupper'];
+  welcomeCupper.$inject = ['$scope','cupper'];
 
-  function welcomeCupper(cupper){
+  function welcomeCupper($scope, cupper){
     var welcome = this;
 
     welcome.cupper = { };
     welcome.newCupper = newCupper;
 
     function newCupper(){
-      return cupper.create(welcome.cupper);
+      return cupper.create(welcome.cupper)
+        .then(function(){
+          welcome.cupper = {};
+          $scope.form.$setPristine();
+        });
     };
   }
 }());

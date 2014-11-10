4664cc0f638e64119e351b5fd1bc1c15b5eee309
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-10 11:51:22 +0100

    Fixing nesting of result of getAll request for cuppers

--- src/server/cupper/api.js
+++ src/server/cupper/api.js
@@ -6,15 +6,15 @@ var cuppers = [];
 
 exports.get = function(req, res) {
 
-  res.json({
-    cuppers : cuppers });
+  res.json(cuppers);
 
 };
 
 exports.post = function(req, res) {
+
   var data = req.body || {};
   var cupper = new Cupper(data.name, data.matriculationNumber);
 
-  cuppers.push(req.body.cupper);
+  cuppers.push(cupper);
   res.status(201).send('Created');
 };

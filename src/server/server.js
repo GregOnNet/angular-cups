1eddf6c00a32014154ecb89f458ab24033f00e9b
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-11 12:37:52 +0100

    Adding getAFreeCup to api

--- src/server/server.js
+++ src/server/server.js
@@ -21,6 +21,7 @@ router.get('/cupper/:matriculationNumber', cupper.getByMatriculationNumber);
 
 router.post('/cupper', cupper.post);
 router.put('/cupper/drinksACup', cupper.drinksACup);
+router.put('/cupper/getsAFreeCup', cupper.getsAFreeCup);
 
 app.use('/api', router);
 

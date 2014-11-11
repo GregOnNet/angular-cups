ab7d6c7a1b80523e6fb9cf7b374d6ec69e0f1301
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-11 08:41:54 +0100

    Removing unused route

--- src/app/cupper/cupper.routes.js
+++ src/app/cupper/cupper.routes.js
@@ -9,10 +9,6 @@
 
   function defineRoutes($routeProvider) {
     $routeProvider
-      .when('/cupper/:matriculationNumber', {
-        templateUrl: 'cupper/cupper.html',
-        controller: 'cupper'
-      })
       .when('/welcome-a-new-cupper', {
         templateUrl: 'cupper/welcomeCupper.html',
         controller: 'welcomeCupper',

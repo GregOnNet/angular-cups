2c7716c4fda6273741740f8ab1ab44adc0083150
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-11 10:16:21 +0100

    Implementing function to get cupper by matriculation number

--- src/server/cupper/api.js
+++ src/server/cupper/api.js
@@ -10,6 +10,27 @@ exports.get = function(req, res) {
 
 };
 
+exports.getByMatriculationNumber = function(req, res) {
+
+  var foundCuppers = cuppers.filter(function(cupper) {
+    return cupper.matriculationNumber === req.params.matriculationNumber;
+  });
+
+  if (foundCuppers.length === 1)
+    res.json(foundCuppers[0]);
+
+  else if (foundCuppers.length === 0)
+    res
+      .status(404)
+      .send('There is no cupper matching the given matriculation number.');
+
+  else
+    res
+      .status(409)
+      .send('There more than one cupper with the same matriculation number.');
+
+}
+
 exports.post = function(req, res) {
 
   var data = req.body || {};

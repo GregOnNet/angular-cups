d337ed0c093581df8a6ecd0e5381f7ea4cbc8e97
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-11 08:41:13 +0100

    Adding view and controller for bonus card

---
+++ src/app/bonus-card/card.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('cups.bonusCard')
+    .controller('card', card);
+
+  card.$inject = ['cupper'];
+
+  function card(cupper){
+    
+  }
+}());

9a357a7c081f3ef6a2a5ebdec2c325c414e6d328
Gregor Woiwode <gregor.woiwode@gmail.com>
2014-11-11 10:39:28 +0100

    Creating a directive displaying a certain cupper

---
+++ src/app/core/cupsCupper.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('cups.core')
+    .directive('cupsCupper', cupper);
+
+  function cupper(){
+    return {
+      restrict    : 'E',
+      templateUrl : 'core/cups-cupper.html',
+      scope       : {
+        cupper                : '=identity'
+      }
+    }
+  }
+}());

ff9bf7f9e3a36f8326f1ac13500010911bbf9c85
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-11-03 17:50:21 +0100

    Refactors api callback-methods

--- src/app/core/cups.core.api.js
+++ src/app/core/cups.core.api.js
@@ -20,58 +20,46 @@
 
     function create(cupper) {
       return $http.post(api, cupper)
-         .then(function(response) {
-           snackbar({content: response.data});
-         })
-         .catch(function(error) {
-           snackbar({content: error.data});
-         });
+         .then(showResponseMessage)
+         .catch(showResponseMessage);
     }
 
     function get(matriculationNumber) {
       return $http.get(api + matriculationNumber)
          .then(returnResponse)
-         .catch(function(error) {
-           snackbar({content: error.data});
-         });
+         .catch(showResponseMessage);
     }
 
     function remove(matriculationNumber) {
       return $http.delete(api + matriculationNumber)
-         .then(function(response) {
-           snackbar({content: response.data});
-         })
-         .catch(function(error) {
-           snackbar({content: error.data});
-         });
+         .then(showResponseMessage)
+         .catch(showResponseMessage);
     }
 
     function getAll() {
       return $http.get('/api/cuppers')
        .then(returnResponse)
-       .catch(function(error) {
-         snackbar({content: error.data});
-       });
+       .catch(showResponseMessage);
     }
 
     function drinksACup(cupper) {
       return $http.put(api + 'drinksACup', cupper)
         .then(returnResponse)
-        .catch(function(error) {
-          snackbar({content: error.data});
-        });
+        .catch(showResponseMessage);
     }
 
     function spendAFreeCup(cupper) {
       return $http.put(api + 'getsAFreeCup', cupper)
         .then(returnResponse)
-        .catch(function(error) {
-          snackbar({content: error.data});
-        });
+        .catch(showResponseMessage);
     }
 
     function returnResponse(response) {
       return response.data;
+    }
+
+    function showResponseMessage(response) {
+      snackbar({content: response.data});
     }
   }
 }());

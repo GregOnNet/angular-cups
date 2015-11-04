229c9115881bb7b30f1b35317ec8fc9ec8d7375a
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-11-04 14:47:37 +0100

    Introduces Expected conditions

--- test/e2e/welcome.spec.js
+++ test/e2e/welcome.spec.js
@@ -11,7 +11,26 @@ describe('When using /welcome', function() {
     ).toBe('Welcome a new cupper');
   });
 
-  
+  describe('When adding a cupper with valid input-values', function() {
 
+    it('should display a "Created"-message', function() {
+      var id = Math.floor(Math.random() * 10000000);
 
-})
+      // Eingabefelder
+      element(by.model('cupper.identity.name'))
+        .sendKeys('Gregor Woiwode');
+      element(by.model('cupper.identity.matriculationNumber'))
+        .sendKeys(id);
+
+      // Add button clicken
+      element(by.buttonText('ADD')).click();
+
+      // Expected Condition, um antwort zu testen...
+      var EC = protractor.ExpectedConditions;
+
+      var toastMessage = element(by.css('span.snackbar-content'));
+      browser.wait(EC.presenceOf(toastMessage), 1000 * 6);
+      expect(toastMessage.getText()).toBe('Created');
+    });
+  });
+});

f9eec6ff947e45d8118b6d3dc236c1d7a40378d8
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-11-04 14:16:43 +0100

    Adds tests for dashboard

---
+++ test/e2e/dashboard.spec.js
@@ -0,0 +1,35 @@
+describe('Serving the dashboard', function() {
+
+  beforeEach(function() {
+    browser.get('/');
+  });
+
+  it('should have a heading "Cups"', function() {
+
+    var heading = element(by.css('h1'));
+    expect(heading.getText()).toBe('Cups');
+
+  });
+
+  it('should have a navigation button to /welcome', function() {
+
+    var navigationButton = element(by.css('a'));
+    expect(navigationButton.isPresent()).toBeTruthy();
+
+  });
+
+  describe('When clicking on the navigation button', function() {
+
+    it('should navigate to /welcome', function() {
+
+      element(by.css('a')).click();
+                                              // #/welcome
+      expect(browser.getCurrentUrl()).toMatch(/#\/welcome/);
+    });
+  });
+
+
+  // Übung
+  // Eingabefeld des filters mit by.model()-Selektor suchen
+  // Prüfen, ob das Eingabefeld im DOM verfügbar ist.
+});
